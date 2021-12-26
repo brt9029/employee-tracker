7173ade7d7585de11ed9025f987da2178099c4f9
brt9029 <brt9029@gmail.com>
2021-12-26 17:41:37 -0600

    initial commit with db and tables

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('James', 'Fraser', 1, 1),
+  ('Jack', 'London', 1, NULL),
+  ('Robert', 'Bruce', 2, 1),
+  ('Derek', 'Jarman', 3, 3),
+  ('Heathcote', 'Williams', 1, 2);
